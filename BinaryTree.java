a7a149ca79c8ecf6ed83ccb3775c57103bc913ee
James543R <144176620+James543R@users.noreply.github.com>
2023-10-04 14:37:47 -0400

    Update BinaryTree.java

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,15 +1,7 @@
 public class BinaryTree<Integer> {
   protected Node<Integer> root;
 
-  //tests if the subClasses override the BinaryTree class
-  public void override(){
-    if(preOrder.getMethod.getDeclaringClass() == BinaryTree){
-      System.out.println("BTPrinting class does not override BinaryTree");
-    }
-    if(getLevel.getMethod.getDeclaringClass() == BinaryTree){
-      System.out.println("searchBT class does not override BinaryTree");
-}
-    if(sumLessThan.getMethod.getDeclaringClass() == BinaryTree){
-      System.out.println("aggregateBT class does not override BinaryTree");
-    }
+  public void testing(){
+    
+  }
 }

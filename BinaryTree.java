001aab5de80c25ade988edf78db6d1d2a22ef023
James543R <144176620+James543R@users.noreply.github.com>
2023-10-04 18:09:06 -0400

    Update BinaryTree.java

--- BinaryTree.java
+++ BinaryTree.java
@@ -2,22 +2,25 @@
 public class BinaryTree {
   protected Node root;
 
+  //Node class to be used with BinaryTree
   static class Node {
-    Node right = null;
-    Node left = null;
+    Node right, left;
     int data;
 
+    //constructor for Node to allow creating new nodes with a right and left node and an int for data
     public Node(Node right, Node left, int data){
       this.right = right;
       this.left = left;
       this.data = data;
     }
 
+    //empty constructor for node
     public Node() {
 
     }
   }
 
+  //empty testing method
   public void testing(){
     
   }

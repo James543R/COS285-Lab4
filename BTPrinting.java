0af3f85f594fa07cabf3be63afef8881197b25e9
mbeal_sgp6gtp <mbeal_sgp6gtp@MasonBealeLaptop.lan>
2023-10-04 18:20:04 -0400

    finished preOrder

--- BTPrinting.java
+++ BTPrinting.java
@@ -1,47 +1,82 @@
+public class BTPrinting extends BinaryTree {
 
-public class BTPrinting extends BinaryTree{
+	/**
+	 * add to a binary search tree
+	 * BTPrinting specific just in case it overlaped with other add methods
+	 * @param key
+	 */
+	public void addBTP(int key)  { 
+        root = addBTP(root, key); 
+    } 
+   
 
+    private Node addBTP(Node localRoot, int num) { 
+
+        if (localRoot == null) { 
+        	localRoot = new Node(num); 
+            return localRoot; 
+        } 
+
+        if (num < localRoot.data)
+        	localRoot.left = addBTP(localRoot.left, num); 
+        else if (num > localRoot.data)   
+        	localRoot.right = addBTP(localRoot.right, num); 
+        return localRoot; 
+    } 
 	/**
 	 * mason beale
 	 */
 	public void preOrder() {
-		System.out.println(root);
-		preOrder(root.left);
-		preOrder(root.right);
+		preOrder(root);
 	}
+
 	/**
 	 * mason beale
+	 * 
 	 * @param n the node to start at
 	 */
 	public void preOrder(Node n) {
-		if(n == null) return;
-		System.out.println(n.data);
+		if (n == null) {
+			return;
+		}
 		preOrder(n.left);
 		preOrder(n.right);
 	}
-	
+
 	/**
 	 * mason beale
 	 */
-	public void printLeft() { 
+	public void printLeft() {
 		printLeft(root.left);
-		System.out.println(root);
+		System.out.println(root.data);
 	}
+
 	/**
 	 * mason beale
+	 * 
 	 * @param n the node to start at
 	 */
-	public void printLeft(Node n) { 
-		if(n == null) return;
+	public void printLeft(Node n) {
+		if (n == null)
+			return;
 		printLeft(n.left);
 		System.out.println(n.data);
 	}
-	
-	public void testing() {
+
+	public static void testing(BTPrinting BTP) {
+		BTP.addBTP(5);
+		BTP.addBTP(3);
+		BTP.addBTP(2);
+		BTP.addBTP(1);
+		BTP.addBTP(4);
+		BTP.addBTP(7);
+		BTP.addBTP(6);
+		BTP.addBTP(8);
+	}
+
+	public static void main(String[] args) {
 		BTPrinting test = new BTPrinting();
-		for(int i = 0;i<10;i++) {
-			test.add(i);
-		}
+		testing(test);
 		test.preOrder();
 	}
 
